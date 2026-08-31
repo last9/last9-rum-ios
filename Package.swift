da2c45222ed36623c911d75f05fb559d8e6b3669
Last9 CI <support@last9.io>
2026-08-31 18:29:12 +0000

    Last9RUM 1.6.9-alpha.33424432692

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Last9RUM",
-            url: "https://cdn.last9.io/rum-sdk/ios/builds/1.6.9-alpha.33424236364/Last9RUM.xcframework.zip",
-            checksum: "737f0507d9918136647e0877c6e3e06a9260485900daaf6719a9f0e4116b250c"
+            url: "https://cdn.last9.io/rum-sdk/ios/builds/1.6.9-alpha.33424432692/Last9RUM.xcframework.zip",
+            checksum: "044643b43ae10b72c67c3fa246d9ad43191933c7bb5993b13de7db6d812d9b9d"
         ),
     ]
 )
